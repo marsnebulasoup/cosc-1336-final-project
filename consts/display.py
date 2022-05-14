7784ba1d3eccfe6eba1378dd5b70c48a527b370e
marsnebulasoup <marsnebulasoup@gmail.com>
2022-05-14 12:05:26 -0500

    Implemented display and stubbed out generate_global/indv._stats

---
+++ consts/display.py
@@ -0,0 +1,9 @@
+######################
+# Name: Kevin Bryniak
+# Final Project
+# Constants used mainly by display functions.
+######################
+
+GLOBAL_STATS_TITLE = "Global Statistics"
+INDIVIDUAL_STATS_TITLE = "Individual Statistics"
+STATS_STYLE ="bold cyan"
--- display/main.py
+++ display/main.py
@@ -4,5 +4,39 @@
 # Generates and displays hotel statistics.
 ######################
 
-def display(hotel_info: DataFrame, amenities: set):
+from pandas import DataFrame
+from rich.console import Console
+from rich.table import Table, Column
+from rich.text import Text
+from rich.padding import Padding
+from consts.display import (
+    GLOBAL_STATS_TITLE,
+    INDIVIDUAL_STATS_TITLE,
+    STATS_STYLE,
+)
+def generate_global_stats(hotel_info: DataFrame, amenities: set):
     pass
+def generate_individual_stats(hotel_info: DataFrame, amenities: set):
+    pass
+def display(hotel_info: DataFrame, amenities: set):
+    if type(hotel_info) is DataFrame and type(amenities) is set:
+        global_stats = generate_global_stats(hotel_info, amenities)
+        individual_stats = generate_individual_stats(hotel_info, amenities)
+
+        grid = Table.grid(
+            Column(),
+            Column(justify="right"),
+            expand=True,
+            padding=(0, 2, 0, 2),
+            pad_edge=True,
+        )
+
+        grid.add_row(
+            Padding(Text(GLOBAL_STATS_TITLE), (1)),
+            Padding(Text(INDIVIDUAL_STATS_TITLE, justify="left"), (1)),
+            style=STATS_STYLE,
+        )
+        grid.add_row(global_stats, individual_stats)
+
+        console = Console()
+        console.print(grid)
