445727c5702d15ae7e0eefd3b8e1e7f052c143cb
marsnebulasoup <marsnebulasoup@gmail.com>
2022-05-14 10:13:45 -0500

    Check for required modules; show error if not found

---
+++ consts/errors.py
@@ -0,0 +1,7 @@
+######################
+# Name: Kevin Bryniak
+# Final Project
+# Error message constants.
+######################
+
+ERROR_MODULES_NOT_FOUND = "Error: Could not find required dependencies. Please install all the required dependencies listed in README.md."
--- main.py
+++ main.py
@@ -3,3 +3,13 @@
 # Final Project
 # Processes CSV files with hotel reviews and outputs useful statistics for them.
 ######################
+
+import sys
+from consts.errors import ERROR_COULD_NOT_FIND_NAME_COL 
+try:
+    from pandas import DataFrame, read_csv
+    import rich
+    from rich.progress import Progress
+except ImportError:
+    print(ERROR_MODULES_NOT_FOUND)
+    sys.exit()
