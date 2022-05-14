b295e4a6609d730d75cb807ff5df3527af8253e9
marsnebulasoup <marsnebulasoup@gmail.com>
2022-05-14 12:11:47 -0500

    Implemented generate_global_amenity_stats and stubbed out get_hotels_with_amenity

--- consts/display.py
+++ consts/display.py
@@ -7,3 +7,7 @@
 GLOBAL_STATS_TITLE = "Global Statistics"
 INDIVIDUAL_STATS_TITLE = "Individual Statistics"
 STATS_STYLE ="bold cyan"
+
+
+UNIQUE_HOTELS_TEXT = "unique hotels"
+AMENITY_TEXT = "Number of hotels w/(a)"
--- display/amenities.py
+++ display/amenities.py
@@ -5,7 +5,24 @@
 ######################
 
 from pandas import DataFrame
+from rich.text import Text
+from consts.display import AMENITY_TEXT
+
+
+def get_hotels_with_amenity(hotel_info: DataFrame, amenity: str):
+    pass
 
 
 def generate_global_amenity_stats(hotel_info: DataFrame, amenities: set):
-    pass
+    text = Text()
+
+    if type(hotel_info) is DataFrame and type(amenities) is set:
+        if not hotel_info.empty and len(amenities):
+            text.append(AMENITY_TEXT)
+
+            sorted_amenities = sorted(amenities)
+            for amenity in sorted_amenities:
+                with_amenity = get_hotels_with_amenity(hotel_info, amenity)
+                text.append(f"\n\t{amenity}: {with_amenity}")
+
+    return text
