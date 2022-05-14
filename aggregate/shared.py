a81d24c955a31db3af1cbcba1126185cab93eaa3
marsnebulasoup <marsnebulasoup@gmail.com>
2022-05-14 10:56:18 -0500

    Implemented get_amenity_existence function

--- aggregate/shared.py
+++ aggregate/shared.py
@@ -5,6 +5,11 @@
 ######################
 
 from pandas import Series
+from consts.aggregate import (
+    AMENITY_DOES_NOT_EXIST, 
+    AMENITY_EXISTS,
+    AMENITY_PATTERN,
+)
 def get_most_freq_acc(series: Series, pattern: str):
     most_freq = -1
     if type(series) is Series and type(pattern) is str and pattern:
@@ -13,3 +18,17 @@ def get_most_freq_acc(series: Series, pattern: str):
             most_freq = most_freq_vals[0]
 
     return most_freq
+
+
+def get_amenity_existence(series: Series):
+    exists = -1
+
+    if type(series) is Series:
+        series = series.astype(str).str.upper()
+        most_freq = get_most_freq_acc(series, AMENITY_PATTERN)
+        if most_freq == AMENITY_EXISTS:
+            exists = 1
+        elif most_freq == AMENITY_DOES_NOT_EXIST:
+            exists = 0
+
+    return exists
